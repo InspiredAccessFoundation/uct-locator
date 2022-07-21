d4e5d4feed7709e501dd7161b22fc14528c0f511
Bella Baglier <isabellabaglier@gmail.com>
2022-07-21 14:04:49 -0400

    updated register ui with mui components

--- front-end/src/components/auth/Register.js
+++ front-end/src/components/auth/Register.js
@@ -4,6 +4,11 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import HomeIcon from '@mui/icons-material/Home';
+import Stack from '@mui/material/Stack';
 
 class Register extends Component {
   constructor() {
@@ -53,96 +58,67 @@ class Register extends Component {
     const { errors } = this.state;
 
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
-            </div>
+      <Container maxWidth='sm'>
+        <div style={{ marginTop: "4rem" }} className="row">
+           <Button variant="outlined" startIcon={<HomeIcon />} href="/">
+                Home
+               </Button>
+              <h2>
+                <b>Register below </b>
+              </h2>
             <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
+            <Stack spacing = {1}>
+              <TextField id='name' variant='outlined' label='Name'
                   onChange={this.onChange}
                   value={this.state.name}
                   error={errors.name}
-                  id="name"
-                  type="text"
                   className={classnames("", {
                     invalid: errors.name
                   })}
                 />
-                <label htmlFor="name">Name</label>
+
                 <span className="red-text">{errors.name}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
+
+              <TextField id='email' variant='outlined' label='Email'
                   onChange={this.onChange}
                   value={this.state.email}
                   error={errors.email}
-                  id="email"
-                  type="email"
                   className={classnames("", {
                     invalid: errors.email
                   })}
                 />
-                <label htmlFor="email">Email</label>
                 <span className="red-text">{errors.email}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
+
+               <TextField id='password' variant='outlined' label='Password' type='password'
                   onChange={this.onChange}
                   value={this.state.password}
                   error={errors.password}
-                  id="password"
-                  type="password"
                   className={classnames("", {
                     invalid: errors.password
                   })}
                 />
-                <label htmlFor="password">Password</label>
                 <span className="red-text">{errors.password}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
+
+                <TextField id='password2' variant='outlined' label='Confirm Password' type="password"
                   onChange={this.onChange}
                   value={this.state.password2}
                   error={errors.password2}
-                  id="password2"
-                  type="password"
                   className={classnames("", {
                     invalid: errors.password2
                   })}
                 />
-                <label htmlFor="password2">Confirm Password</label>
                 <span className="red-text">{errors.password2}</span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Sign up
-                </button>
-              </div>
+              </Stack>
+                <Button variant="contained" type="subimt">
+                  Sign Up
+                </Button>
+                <p className="grey-text text-darken-1">
+                Already have an account? 
+                <Button href='/login'>Login</Button>
+              </p>
             </form>
-          </div>
-        </div>
-      </div>
+            </div>
+        </Container>
     );
   }
 }
