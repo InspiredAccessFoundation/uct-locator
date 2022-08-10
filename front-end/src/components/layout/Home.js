4a2dbd65176c80440f95382a55af6ba6c0a38e58
Brendon Roberto <Brendon.Roberto@hyland.com>
2022-08-10 17:17:46 -0400

    feature: add breadcrumbs for navigation
    * Add Breadcrumbs component using MUI Breadcrumbs
    * Add Breadcrumbs as an always-matched route so every page renders them
    * Add Link component to make it easier to use react-router-dom Links wrapped in MUI Links
    * Add Containers to all pages to constrain to md maxWidth
    * Run prettier formatting on every changed file

--- front-end/src/components/layout/Home.js
+++ front-end/src/components/layout/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { Container } from "@mui/system";
 
 const Home = (props) => {
   const loggedIn = props.auth.isAuthenticated;
@@ -9,7 +10,9 @@ const Home = (props) => {
   const loggedInContent = (
     <>
       <p>You are logged into the application 😉</p>
-      <p><Link to="/submit-table">Submit a Table</Link></p>
+      <p>
+        <Link to="/submit-table">Submit a Table</Link>
+      </p>
     </>
   );
 
@@ -17,23 +20,23 @@ const Home = (props) => {
     <p>
       <Link to="/login">Login</Link> or <Link to="/register">Register</Link>
     </p>
-  )
+  );
 
   return (
-    <div>
+    <Container maxWidth="md">
       <p>Welcome to the UCT Locator app!</p>
-      {loggedIn ? loggedInContent : loggedOutContent }
+      {loggedIn ? loggedInContent : loggedOutContent}
       <Link to="/find">Find a Table</Link>
-    </div>
-  )
-}
-
-Home.propTypes = {
-  auth: PropTypes.object.isRequired
+    </Container>
+  );
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth
+Home.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps)(Home);
---
+++ front-end/src/components/layout/Link.js
@@ -0,0 +1,6 @@
+import MuiLink from "@mui/material/Link"
+import { Link as RouterLink } from "react-router-dom";
+
+export default function Link(props) {
+	return <MuiLink {...props} component={RouterLink} />;
+}
