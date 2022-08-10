be4b707f32779e3f83ab817af46197771191d9a5
Brendon Roberto <Brendon.Roberto@hyland.com>
2022-08-10 18:10:50 -0400

    chore: fix formatting in Link.js

--- front-end/src/components/layout/Link.js
+++ front-end/src/components/layout/Link.js
@@ -2,5 +2,5 @@ import MuiLink from "@mui/material/Link"
 import { Link as RouterLink } from "react-router-dom";
 
 export default function Link(props) {
-	return <MuiLink {...props} component={RouterLink} />;
+  return <MuiLink {...props} component={RouterLink} />;
 }
