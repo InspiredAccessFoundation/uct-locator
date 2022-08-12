21c4a77fdb224d49920ab6e4089bdc6167c0d946
Aakarsh <aakarsh0607@gmail.com>
2022-08-12 11:40:54 -0400

    Everything works other than the pop up which I need help with

--- front-end/src/components/Pictures/SubmitPicture.js
+++ front-end/src/components/Pictures/SubmitPicture.js
@@ -14,84 +14,80 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import React, { useState } from 'react';
 import axios from "axios";
-
-
+import { useParams } from 'react-router-dom';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, Link } from '@mui/material';
+ 
+ 
 const SubmitPicture = () => {
-
-    const theme = useTheme();
-    const smallWidth = useMediaQuery(theme.breakpoints.down('sm'));
-    let tableMapStyle = {};
-
-    if (smallWidth) {
-        tableMapStyle.height = "350px";
-    }
-
-    const [name, setName] = useState("")
-    const [url, getUrl] = useState("")
-    const { tableId } = useParams();
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newPicture = {
-          name: name,
-          url: url,
-          tableId: tableId
-
-        };
-        axios
-        .post("/api/pictures/submit", newPicture)
-        .then((res) => {
-          console.log(res.data)
-      }).catch((error) => {
-        alert("something went wrong");
-    
-          console.log(error)
-      });
-       
-    }
-
-    return (
-        <>
-          <Container maxWidth="md">
-            <div style={{ marginTop: "1rem" }} className="row">
-              <h2>Submit Picture</h2>
-              <p>Enter information for your picture here.</p>
-              <form className='form' onSubmit={handleSubmit}>
-                <Stack spacing={1}>
-                  <TextField id="name" label="Name" variant="outlined"
-                    onChange={(e) => setName(e.target.value)}
-                    value={name} required
-                  />
-                  <TextField id="url" label="URL" variant="outlined"
-                    onChange={(e) => setUrl(e.target.value)}
-                    value={url}
-                  />
-                  <Button
-                    variant="contained"
-                    type="submit"
-                  >
-                    Submit
-                  </Button>
-                </Stack>
-              </form>
-            </div>
-          </Container>
-          <Dialog
-            // open={!!submittedTableId}
-            // onClose={handleSuccessClose}
-            align="center"
-          >
-            <DialogTitle>Success!</DialogTitle>
-            <DialogContent>
-              <DialogContentText sx={{ paddingBottom: "1em" }}>
-                Your picture has been submitted.
-              </DialogContentText>
-              <DialogContentText sx={{ paddingBottom: ".5em" }}>
-                <Link href={`/submit-picture/${TableId}`}>View the picture</Link>
-              </DialogContentText>
-            </DialogContent>
-          </Dialog>
-        </>
-      )
+ 
+   const [name, setName] = useState("")
+   const [url, setUrl] = useState("")
+   const { tableId } = useParams();
+ 
+   const handleSubmit = (e) => {
+       e.preventDefault();
+       const newPicture = {
+         name: name,
+         url: url,
+         tableId: tableId
+ 
+       };
+       axios
+       .post("/api/pictures/submit", newPicture)
+       .then((res) => {
+         console.log(res.data)
+         alert("success!")
+     }).catch((error) => {
+       alert("something went wrong");
+  
+         console.log(error)
+     });
+     
+   }
+ 
+   return (
+       <>
+         <Container maxWidth="md">
+           <div style={{ marginTop: "1rem" }} className="row">
+             <h2>Submit Picture</h2>
+             <p>Enter information for your picture here.</p>
+             <form className='form' onSubmit={handleSubmit}>
+               <Stack spacing={1}>
+                 <TextField id="name" label="Name" variant="outlined"
+                   onChange={(e) => setName(e.target.value)}
+                   value={name} required
+                 />
+                 <TextField id="url" label="URL" variant="outlined"
+                   onChange={(e) => setUrl(e.target.value)}
+                   value={url}
+                 />
+                 <Button
+                   variant="contained"
+                   type="submit"
+                 >
+                   Submit
+                 </Button>
+               </Stack>
+             </form>
+           </div>
+         </Container>
+         <Dialog
+           // open={!!submittedTableId}
+           // onClose={handleSuccessClose}
+           align="center"
+         >
+           <DialogTitle>Success!</DialogTitle>
+           <DialogContent>
+             <DialogContentText sx={{ paddingBottom: "1em" }}>
+               Your picture has been submitted.
+             </DialogContentText>
+             <DialogContentText sx={{ paddingBottom: ".5em" }}>
+               <Link href={`/view-table/${tableId}`}>View the table</Link>
+             </DialogContentText>
+           </DialogContent>
+         </Dialog>
+       </>
+     )
 }
 export default SubmitPicture;
+ 
