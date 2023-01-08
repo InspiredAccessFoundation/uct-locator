c51892ba6368d19a015484bf009d829b842225bd
Nathan Zender <github@nathanzender.com>
2023-01-07 20:49:15 -0500

    Add a down function

--- back-end/migrations/2-delete-points-db.js
+++ back-end/migrations/2-delete-points-db.js
@@ -21,17 +21,62 @@ var migrationCommands = [{
     params: ["points"]
 }];
 
+var downMigrationCommands = [{
+    fn: "createTable",
+    params: [
+        "points",
+        {
+            "id": {
+                "type": Sequelize.INTEGER,
+                "field": "id",
+                "primaryKey": true,
+                "autoIncrement": true
+            },
+            "coordinates": {
+                "type": Sequelize.GEOMETRY('POINT', 4326),
+                "field": "coordinates",
+                "allowNull": false
+            },
+            "createdAt": {
+                "type": Sequelize.DATE,
+                "field": "createdAt",
+                "allowNull": false
+            },
+            "updatedAt": {
+                "type": Sequelize.DATE,
+                "field": "updatedAt",
+                "allowNull": false
+            }
+        },
+        {}
+    ]
+}]
+
 module.exports = {
     pos: 0,
-    up: function(queryInterface, Sequelize)
-    {
+    up: function (queryInterface, Sequelize) {
         var index = this.pos;
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             function next() {
-                if (index < migrationCommands.length)
-                {
+                if (index < migrationCommands.length) {
                     let command = migrationCommands[index];
-                    console.log("[#"+index+"] execute: " + command.fn);
+                    console.log("[#" + index + "] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    down: function (queryInterface, Sequelize) {
+        var index = this.pos;
+        return new Promise(function (resolve, reject) {
+            function next() {
+                if (index < downMigrationCommands.length) {
+                    let command = downMigrationCommands[index];
+                    console.log("[#" + index + "] execute: " + command.fn);
                     index++;
                     queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
                 }
