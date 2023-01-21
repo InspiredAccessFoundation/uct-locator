416b2b38c9f71d9a1a02242e48c5f5b53c9cad7c
Nathan Zender <github@nathanzender.com>
2023-01-21 10:47:40 -0500

    Add migrate env

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,10 +1,28 @@
+require('dotenv').config();
+
 module.exports = {
   local: {
-    username: "uct-user",
-    password: "uct-password",
-    database: "postgres",
-    host: "127.0.0.1",
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: "postgres"
+  },
+  // Pulled from AWS Secrets Manager and stored in ENV vars
+  migrate: {
+    username: process.env.POSTGRES_ADMIN_USERNAME,
+    password: process.env.POSTGRES_ADMIN_PASSWORD,
+    database: process.env.POSTGRES_ADMIN_DB,
+    host: process.env.POSTGRES_ADMIN_HOST,
+    port: process.env.POSTGRES_ADMIN_PORT,
+    dialect: "postgres",
+    pool: {
+      max: 1,
+      min: 1,
+      acquire: 30000,
+      idle: 10000
+    }
   },
   development: {
     username: process.env.POSTGRES_USERNAME,
