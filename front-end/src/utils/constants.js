32979679222a5dd91074bfcd7e9efb5834b3d236
zendern <zendern@users.noreply.github.com>
2023-02-08 02:53:08 +0000

    Update gitbook 2023-02-08 02:53:08

--- front-end/src/utils/constants.js
+++ front-end/src/utils/constants.js
@@ -6,12 +6,12 @@ export const macOS = typeof navigator !== 'undefined' && navigator.platform.inde
 // Allows for configuration of differnt api keys per environment. 
 export const GoogleApiKey = () => {
     if (window.location.hostname.startsWith("dev")) {
-        return process.env.DEV_REACT_APP_GOOGLE_MAPS_API_KEY
+        return process.env.REACT_APP_DEV_GOOGLE_MAPS_API_KEY;
     } else if (window.location.hostname.startsWith("app")) {
-        return process.env.PROD_REACT_APP_GOOGLE_MAPS_API_KEY
+        return process.env.REACT_APP_PROD_GOOGLE_MAPS_API_KEY;
     } else if (process.env.REACT_APP_GOOGLE_MAPS_API_KEY) {
         // Locally we expected this value to be set in .env
-        return process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+        return process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     } else if (process.env.NODE_ENV === "test") {
         return "dummy-test-value"
     } else {
